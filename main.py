c38ff7ecb03918e5f42593000b9994c3a2ba335a
XuMuK666 <dedelya666@gmail.com>
2020-02-16 14:35:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,15 @@ from pyunpack import Archive
 import magic
 import os
 from shutil import copyfile
+try:
+    import lzma
+except ImportError:
+    from backports import lzma
 
 def rename():
     f = magic.Magic(uncompress=True)
     info = f.from_file("flag.txt")
+    print(info)
     fileopen =""
     if info.find("POSIX") !=-1:
         fileopen="flag.tar.gz"
@@ -32,6 +37,9 @@ def rename():
         copyfile("flag.txt", fileopen)
         os.remove("flag.txt")
 
+        print (lzma.open('flag.tar.xz').read())
+        fileopen=""
+
     if len(fileopen)>0:
         extract(fileopen)
 
@@ -39,6 +47,11 @@ def extract(fileopen):
 
     Archive(fileopen).extractall('./test/')
     os.remove(fileopen)
+    try:
+        copyfile("./test/flag", "./test/flag.txt")
+        os.remove("./test/flag")
+    except:
+        print("small error")
     copyfile("./test/flag.txt", "flag.txt")
     os.remove("./test/flag.txt")
 
